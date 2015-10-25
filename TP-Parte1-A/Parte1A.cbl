e302c6aadbcd5532e1c4c739ef752c336904fa4e
Adrián Mouly <adrian@mouly.com.ar>
2015-10-25 20:29:43 -0300

    Agregado archivos de IN y OUT

---
+++ TP-Parte1-A/Parte1A.cbl
@@ -0,0 +1,42 @@
+      ******************************************************************
+      * Author:
+      * Date:
+      * Purpose:
+      * Tectonics: cobc
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+      *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+       PROGRAM-ID. "TP_PARTE_1A".
+       AUTHOR. "Adrian Mouly - Sebastian Torres".
+       DATE-WRITTEN. "1er cuatrimestre 2014".
+
+       ENVIRONMENT DIVISION.
+      *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+       CONFIGURATION SECTION.
+      *-----------------------
+       INPUT-OUTPUT SECTION.
+      *-----------------------
+
+       FILE-CONTROL.
+           SELECT NOVTIMES1 ASSIGN TO INPUT
+           "./files/in/NovTimes1.dat"
+           ORGANIZATION IS LINE SEQUENTIAL
+           FILE STATUS IS ALQUILERES-STATUS.
+
+
+       DATA DIVISION.
+      *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+       FILE SECTION.
+      *-----------------------
+       WORKING-STORAGE SECTION.
+      *-----------------------
+       PROCEDURE DIVISION.
+      *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
+       MAIN-PROCEDURE.
+      **
+      * The main procedure of the program
+      **
+            DISPLAY "Hello world"
+            STOP RUN.
+      ** add other procedures here
+       END PROGRAM "TP_PARTE_1A".
