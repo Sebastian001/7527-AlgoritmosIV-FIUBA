d81951193b87a4e01c043315ed00c6b9fe1fadb0
Adrián Mouly <adrian@mouly.com.ar>
2015-10-30 00:11:57 -0300

    Lectura de NovTimes y Profesores

--- TP-Parte1-A/Parte1A.cbl
+++ TP-Parte1-A/Parte1A.cbl
@@ -2,7 +2,7 @@
       *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
        PROGRAM-ID. "TP_PARTE_1A".
        AUTHOR. "Adrian Mouly - Sebastian Torres".
-       DATE-WRITTEN. "1er cuatrimestre 2014".
+       DATE-WRITTEN. "2do cuatrimestre 2015".
 
        ENVIRONMENT DIVISION.
       *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
@@ -73,6 +73,24 @@
       *-----------------------
        WORKING-STORAGE SECTION.
 
+       77 SUCURSALES-EOF    PIC X(2)      VALUE "NO".
+          88 EOF                          VALUE "SI".
+
+       77 TIPOSCLASE-EOF    PIC X(2)      VALUE "NO".
+          88 EOF                          VALUE "SI".
+
+       77 NOVTIMES1-EOF     PIC X(2)      VALUE "NO".
+          88 EOF                          VALUE "SI".
+
+       77 NOVTIMES2-EOF     PIC X(2)      VALUE "NO".
+          88 EOF                          VALUE "SI".
+
+       77 NOVTIMES3-EOF     PIC X(2)      VALUE "NO".
+          88 EOF                          VALUE "SI".
+
+       77 PROFESORES-EOF    PIC X(2)      VALUE "NO".
+          88 EOF                          VALUE "SI".
+
        77 FS-NOVTIMES1      PIC X(2).
        77 FS-NOVTIMES2      PIC X(2).
        77 FS-NOVTIMES3      PIC X(2).
@@ -81,6 +99,27 @@
        77 FS-TIPOSCLASE     PIC X(2).
        77 FS-TIMES          PIC X(2).
 
+       01 FECHA-DE-HOY.
+           03  FECHA-AAAA      pic 9(4).
+           03  FECHA-MM        pic 9(2).
+           03  FECHA-DD        pic 9(2).
+
+       01 ENCABEZADO1.
+           03  FILLER      PIC X(9)    VALUE "Fecha: ".
+           03  FECHA-DD    PIC 9(2).
+           03  FILLER      PIC X       VALUE "/".
+           03  FECHA-MM    PIC 9(2).
+           03  FILLER      PIC X       VALUE "/".
+           03  FECHA-AAAA  PIC 9(4).
+           03  FILLER      PIC X(50)   VALUE SPACES.
+           03  FILLER      PIC X(6)    VALUE "Hoja: ".
+           03  E1-HOJA     PIC 9(3).
+
+       01 ENCABEZADO2.
+           03 FILLER PIC x(26) VALUE SPACES.
+           03 FILLER PIC X(38) VALUE "Listado de horas aplicadas".
+           03 FILLER PIC x(26) VALUE SPACES.
+
       *-----------------------
        PROCEDURE DIVISION.
       *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
@@ -88,14 +127,29 @@
       *- INICIO LLAMADO A PROCEDIMIENTOS
 
            PERFORM INICIALIZAR.
-           PERFORM LEER-SUCURSALES.
+           PERFORM PRINT-ENCABEZADO.
+           PERFORM ABRIR-ARCHIVOS.
+
+           PERFORM LEER-TIPOSCLASE.
+           PERFORM LEER-NOVTIMES1.
+           PERFORM LEER-NOVTIMES2.
+           PERFORM LEER-NOVTIMES3.
+
            PERFORM CERRAR-ARCHIVOS.
-           DISPLAY "Programa Ejecutado".
            STOP RUN.
 
       *- FIN LLAMADO A PROCEDIMIENTOS
 
        INICIALIZAR.
+           DISPLAY "Inicializar Variables".
+
+       PRINT-ENCABEZADO.
+           MOVE FUNCTION CURRENT-DATE TO FECHA-DE-HOY.
+           MOVE CORRESPONDING FECHA-DE-HOY TO ENCABEZADO1.
+           DISPLAY ENCABEZADO1.
+           DISPLAY ENCABEZADO2.
+
+       ABRIR-ARCHIVOS.
            OPEN INPUT NOVTIMES1_FILE.
            IF FS-NOVTIMES1 IS NOT EQUAL TO 00
                DISPLAY "ERROR AL ABRIR NOVTIMES1 FS: " FS-NOVTIMES1
@@ -140,14 +194,42 @@
 
        LEER-SUCURSALES.
            READ SUCURSALES_FILE
-           RECORD AT END MOVE HIGH-VALUE TO SUC-SUCURSAL.
+           RECORD AT END MOVE "SI" TO SUCURSALES-EOF.
            IF FS-SUCURSALES IS NOT EQUAL TO 00 AND 10
                DISPLAY "ERROR AL LEER SUCURSALES FS: " FS-SUCURSALES
            END-IF.
 
+       LEER-NOVTIMES1.
+           READ NOVTIMES1_FILE
+           RECORD AT END MOVE "SI" TO NOVTIMES1-EOF.
+           IF FS-NOVTIMES1 IS NOT EQUAL TO 00 AND 10
+               DISPLAY "ERROR AL LEER NOVTIMES1 FS: " FS-NOVTIMES1
+           END-IF.
+
+       LEER-NOVTIMES2.
+           READ NOVTIMES2_FILE
+           RECORD AT END MOVE "SI" TO NOVTIMES2-EOF.
+           IF FS-NOVTIMES2 IS NOT EQUAL TO 00 AND 10
+               DISPLAY "ERROR AL LEER NOVTIMES2 FS: " FS-NOVTIMES2
+           END-IF.
+
+       LEER-NOVTIMES3.
+           READ NOVTIMES3_FILE
+           RECORD AT END MOVE "SI" TO NOVTIMES3-EOF.
+           IF FS-NOVTIMES3 IS NOT EQUAL TO 00 AND 10
+               DISPLAY "ERROR AL LEER NOVTIMES3 FS: " FS-NOVTIMES3
+           END-IF.
+
+       LEER-PROFESORES.
+           READ PROFESORES_FILE
+           RECORD AT END MOVE "SI" TO PROFESORES-EOF.
+           IF FS-PROFESORES IS NOT EQUAL TO 00 AND 10
+               DISPLAY "ERROR AL LEER PROFESORES FS: " FS-PROFESORES
+           END-IF.
+
        LEER-TIPOSCLASE.
            READ TIPOSCLASE_FILE
-           RECORD AT END MOVE HIGH-VALUE TO TIP-CLASE.
+           RECORD AT END MOVE "SI" TO TIPOSCLASE-EOF.
            IF FS-TIPOSCLASE IS NOT EQUAL TO 00 AND 10
                DISPLAY "ERROR AL LEER TIPOS-CLASE FS: " FS-TIPOSCLASE
            END-IF.
